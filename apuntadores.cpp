30107d97b3fe8d953007c150c3aaf82bc5e34899
Israel Argueta Luna <isarargueta@gmail.com>
2018-08-15 08:49:18 -0500

    Agregando otro archivo

---
+++ apuntadores.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int arreglo[]={1,2,3,4,5};
+int *p;
+	int *puntero;
+int main (){
+	puntero=&arreglo[0];
+	cout<<"Arreglo directo"<<p<<"\n";
+	
+	
+	return 0;
+}
